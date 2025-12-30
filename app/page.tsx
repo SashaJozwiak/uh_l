ee70003b0379e8bc428a90e50e2875603b280726
SashaJozwiak <wmdz88@gmail.com>
2025-12-30 16:38:10 +0500

    addicons

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,11 @@ export default function Home() {
             </p>
             <div className={styles.features}>
               <div className={styles.feature}>
-                <div className={styles.featureIcon}>💼</div>
+                <div className={styles.featureIcon}>{/* 💼 */}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={'3rem'} className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+</svg>
+                </div>
                 <div className={styles.featureText}>
                   <strong>{t('feature.strategic.title')}</strong>
                   <span>{t('feature.strategic.desc')}</span>
@@ -87,7 +91,12 @@ export default function Home() {
                 </div>
               </div> */}
               <div className={styles.feature}>
-                <div className={styles.featureIcon}>🔥</div>
+                <div className={styles.featureIcon}>{/* 🔥 */}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} width={'3rem'} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
+</svg>
+                </div>
                 <div className={styles.featureText}>
                   <strong>{t('feature.burn.title')}</strong>
                   <span>{t('feature.burn.desc')}</span>
